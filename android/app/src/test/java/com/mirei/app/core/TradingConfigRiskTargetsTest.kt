bd1d69e7a42c0c5ec57cf4d06fa16f4d858c086a
doushitekana62-ship-it <doushitekana62@gmail.com>
2026-09-10 14:10:34 +0700

    fix(test): use project JUnit assertions

--- android/app/src/test/java/com/mirei/app/core/TradingConfigRiskTargetsTest.kt
+++ android/app/src/test/java/com/mirei/app/core/TradingConfigRiskTargetsTest.kt
@@ -1,7 +1,7 @@
 package com.mirei.app.core
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class TradingConfigRiskTargetsTest {
     @Test
